de5891ff5d3cbf90b460fc673c4073b59174dff6
Misserine C <misserine.carvajal@gmail.com>
2023-08-07 21:27:09 -0300

    :sparkles: instalado bootstrap, jquery, criados componentes, models e services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ConversorModule } from './conversor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ConversorModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/conversor/models/conversao-response.model.ts
@@ -0,0 +1,10 @@
+export class ConversaoResponse {
+
+    constructor(
+        public base: string,
+        public date: string,
+        public rates: any
+    ) {}
+
+    
+}
